39c6352b1be18ce89cf1380165c28f5454e164b3
Crazy <dmitrijs.braslis@gmail.com>
2020-07-12 01:41:52 +0300

    123

--- src/test/java/StartTest.java
+++ src/test/java/StartTest.java
@@ -1,9 +1,12 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class StartTest {
 
     @Test
     public void startTest() {
+        WebDriver driver = new ChromeDriver();
         System.out.println("murk");
     }
 }
